7590eb492c5caeaf7a12fab6db5c457b7b62d3d8
Nishant Sharma <n.sharma@enterprisedb.com>
2025-05-08 13:02:54 +0100

    Adding fallback to all git version extraction statements

--- ci/scripts/build-go-binaries.bash
+++ ci/scripts/build-go-binaries.bash
@@ -12,8 +12,11 @@ fi
 # Build gpbackup
 pushd ${GOPATH}/src/github.com/greenplum-db/gpbackup
   make depend build unit
-  version=$(git describe --tags | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
+  # version=$(git describe --tags | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
+  version=$( (git describe --tags 2>/dev/null || git rev-parse --short HEAD) | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
+
 popd
+echo "DEBUG_NS: Printing value of version: ${version}"
 echo ${version} > go_components/gpbackup_version
 
 if [[ "gpbackup version ${version}" != "$(${GOPATH}/bin/gpbackup --version)" ]]; then
@@ -25,7 +28,7 @@ fi
 # Build s3 plugin
 pushd ${GOPATH}/src/github.com/greenplum-db/gpbackup-s3-plugin
   make depend build unit
-  s3_plugin_version=$(git describe --tags | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
+  s3_plugin_version=$( (git describe --tags 2>/dev/null || git rev-parse --short HEAD) | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
 popd
 echo ${s3_plugin_version} > go_components/s3_plugin_version
 
@@ -38,7 +41,7 @@ fi
 # Build gpbackup-manager
 pushd ${GOPATH}/src/github.com/pivotal/gp-backup-manager
   make depend build unit
-  gpbackup_manager_version=$(git describe --tags | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
+  gpbackup_manager_version=$( (git describe --tags 2>/dev/null || git rev-parse --short HEAD) | perl -pe 's/(.*)-([0-9]*)-(g[0-9a-f]*)/\1+dev.\2.\3/')
 popd
 echo ${gpbackup_manager_version} > go_components/gpbackup_manager_version
 
